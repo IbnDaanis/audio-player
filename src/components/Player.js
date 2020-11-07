b44e20cee20817458e126368f9cb2c211c879ae7
Ikraam <IbnDaanis@Gmail.com>
2020-11-07 19:55:42 +0000

    Bug fixed

--- src/components/Player.js
+++ src/components/Player.js
@@ -24,8 +24,8 @@ export const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   }
 
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null
+    currentTime: 0,
+    duration: 0
   })
 
   const timeUpdateHandler = e => {
