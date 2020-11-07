a3547e787854a14ace76a72590f6acbec3e0fdb8
Ikraam <IbnDaanis@Gmail.com>
2020-11-07 18:05:56 +0000

    Added songs to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import { Player } from './components/Player'
-import { Audio } from './components/Audio'
+import { Song } from './components/Song'
 import './styles/app.scss'
+import data from './data'
 export const App = () => {
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
   return (
     <div>
-      <Audio />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   )
--- src/components/Audio.js
+++
@@ -1,9 +0,0 @@
-export const Audio = () => {
-  return (
-    <div className='audio-container'>
-      <h1>Picture</h1>
-      <h1>Song Name</h1>
-      <h1>Artist</h1>
-    </div>
-  )
-}
