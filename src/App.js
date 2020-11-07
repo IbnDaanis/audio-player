b196462b98cb6b021079928f8c6bb834727d85f6
Ikraam <IbnDaanis@Gmail.com>
2020-11-07 17:22:21 +0000

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-
+import { Player } from './components/Player'
+import { Audio } from './components/Audio'
 export const App = () => {
   return (
     <div>
       <h1>Audio Player</h1>
+      <Audio />
+      <Player />
     </div>
   )
 }
---
+++ src/components/Audio.js
@@ -0,0 +1,9 @@
+export const Audio = () => {
+  return (
+    <div className='audio-container'>
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  )
+}
